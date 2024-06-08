54da46ca6a3e5dc7dbc81e1d235cd0da2889aed6
soumik9 <soumik.ahammed.9@gmail.com>
2024-06-09 00:46:31 +0600

    intial files

---
+++ utils/helpers/global/index.js
@@ -0,0 +1,3 @@
+export { default as catchAsync } from './catchAsync.js';
+export { default as getRequestUrl } from './sendResponse.js';
+export { default as sendResponse } from './sendResponse.js';
---
+++ utils/server/globalMiddlewares.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from 'cors'
+import helmet from "helmet";
+import xss from 'xss-clean'
+import sanitize from 'express-mongo-sanitize'
+import upload from "../multer/configMulter.js";
+
+const globalMiddleware = (app) => {
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(sanitize());
+    app.use(xss());
+    app.use(helmet());
+
+    // multer configure
+    app.use(
+        upload.fields([
+            { name: "single", maxCount: 1 },
+            { name: "multiple", maxCount: 10 },
+        ])
+    );
+}
+
+export default globalMiddleware;
